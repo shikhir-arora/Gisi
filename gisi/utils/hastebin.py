86265b5324aea52d6fa689f0ae4bb55b0a2560a7
Shikhir Arora <shikhir@thesarogroup.com>
2018-05-05 00:56:21 -0400

    Stop being lazy

--- gisi/utils/hastebin.py
+++ gisi/utils/hastebin.py
@@ -1,5 +1,17 @@
-import requests
+import aiohttp
 
-async def post(content):
-    post = requests.post("https://hastebin.com/documents", data=content.encode('utf-8'))
-    return "https://hastebin.com/" + post.json()["key"]
+async def post(session, content):
+    """Upload content to hastebin.
+    :param session: aiohttp ClientSession to use or new
+    :param content: content to upload
+    :returns: url to post or error
+    """ 
+    if not session:
+        session = aiohttp.ClientSession()
+     
+    async with session.post("https://hastebin.com/documents", data=content.encode("utf-8")) as resp:
+        if resp.status == 200:
+            data = await resp.json()
+            return "https://hastebin.com/{}".format(data["key"])
+        else:
+            return f"Error with Hastebin. Server reported error code {resp.status} - RIP."
