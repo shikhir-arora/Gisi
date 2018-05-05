fd060766345b2531ba87ba2da4e031d6a2aac356
Shikhir Arora <shikhir@thesarogroup.com>
2018-05-05 18:37:55 -0400

    Cleanup

--- gisi/utils/hastebin.py
+++ gisi/utils/hastebin.py
@@ -1,14 +1,9 @@
-import aiohttp
-
 async def post(session, content):
     """Upload content to hastebin.
-    :param session: aiohttp ClientSession to use or new
+    :param session: aiohttp ClientSession to use 
     :param content: content to upload
     :returns: url to post or error
-    """ 
-    if not session:
-        session = aiohttp.ClientSession()
-     
+    """     
     async with session.post("https://hastebin.com/documents", data=content.encode("utf-8")) as resp:
         if resp.status == 200:
             data = await resp.json()
