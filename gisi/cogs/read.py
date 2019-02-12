3967cc758c51ad3d291c53098b0d8f3207d98fb8
Shikhir Arora <shikhir@giesela.io>
2019-02-11 22:07:00 -0500

    Mark as read

---
+++ gisi/cogs/read.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import logging
+
+log = logging.getLogger(__name__)
+  
+class Markread:
+    """Marks stuff as read or something.
+    """
+    
+    def __init__(self, bot):
+        self.bot = bot
+                   
+    @command
+    async def read(self, ctx, id: int=None):
+        """Marks a specified server as read. If an ID is not provided, all servers will be marked as read."""
+        await ctx.message.delete()
+        if id:
+            guild = self.bot.get_guild(int(id))
+            if guild:
+                await guild.ack()
+                await ctx.send("Marked guild {} as read.".format(guild.name))
+            else:
+                await ctx.send("Invalid server ID.")
+        else:
+            for guild in self.bot.guilds:
+                await guild.ack()
+            await ctx.send("Marked all {} guilds as read.".format(len(self.bot.guilds)))
+
+def setup(bot):
+    bot.add_cog(Read(bot))
